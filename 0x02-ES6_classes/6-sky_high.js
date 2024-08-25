088239d2fe022428f8dc5aac41ebf577a21b1884
wallacesintra <wallvxwallace962@gmail.com>
2024-08-25 16:45:24 +0300

    inheritance

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,27 @@
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors;
+  }
+
+  set floors(floors) {
+    if (typeof floors !== 'number') {
+      throw new TypeError('floors must be a number');
+    }
+    this._floors = floors;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this.floors} floors`;
+  }
+}
