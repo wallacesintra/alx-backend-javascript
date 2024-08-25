cbc0f8394fb2898dbe18515faca20afc609822e2
wallacesintra <wallvxwallace962@gmail.com>
2024-08-25 18:56:19 +0300

    primitive

--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -4,6 +4,22 @@ class HolbertonClass {
     this._location = location;
   }
 
+  get size() {
+    return this._size;
+  }
+
+  set size(value) {
+    this._size = value;
+  }
+
+  get location() {
+    return this._location;
+  }
+
+  set location(value) {
+    this._location = value;
+  }
+
   [Symbol.toPrimitive](hint) {
     if (hint === 'number') {
       return this.size;
