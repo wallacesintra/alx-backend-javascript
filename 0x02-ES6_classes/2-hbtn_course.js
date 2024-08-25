34650ccdaeb8cba46c624fab3b8408e1e1a5e521
wallacesintra <wallvxwallace962@gmail.com>
2024-08-25 15:37:37 +0300

    getters and setters

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,42 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  set name(name) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+
+    this._name = name;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set length(length) {
+    if (typeof length !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+
+    this._length = length;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set students(students) {
+    if (students instanceof Array) {
+      this._students = students;
+    }
+    throw new TypeError('Students must an array');
+  }
+
+  get students() {
+    return this._students;
+  }
+}
