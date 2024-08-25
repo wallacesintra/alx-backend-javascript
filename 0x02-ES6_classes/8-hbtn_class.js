b05500edd2c59c781979785d8ce3ae274e4ce0dc
wallacesintra <wallvxwallace962@gmail.com>
2024-08-25 18:09:37 +0300

    primitive

---
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -0,0 +1,18 @@
+class HolbertonClass {
+  constructor(size, location) {
+    this._size = size;
+    this._location = location;
+  }
+
+  [Symbol.toPrimitive](hint) {
+    if (hint === 'number') {
+      return this.size;
+    }
+    if (hint === 'string') {
+      return this.location;
+    }
+    return this;
+  }
+}
+
+export default HolbertonClass;
