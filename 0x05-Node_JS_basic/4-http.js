a309df726631b4baa283a38899595c76e3a6df3b
wallacesintra <wallvxwallace962@gmail.com>
2024-09-29 21:18:59 +0300

    express

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -7,18 +7,21 @@
 
 const http = require('http');
 
+const PORT = 1245;
+const HOST = 'localhost';
 const app = http.createServer();
 
-// eslint-disable-next-line no-unused-vars
-app.on('request', (res, _) => {
-  const text = 'Hello Holberton School!';
+app.on('request', (_, res) => {
+  const responseText = 'Hello Holberton School!';
 
   res.setHeader('Content-Type', 'text/plain');
-  res.setHeader('Content-Length', text.length);
-  res.statusCode(200);
-  res.write(Buffer.from(text));
+  res.setHeader('Content-Length', responseText.length);
+  res.statusCode = 200;
+  res.write(Buffer.from(responseText));
 });
 
-app.listen(1245, 'localhost', () => {
-  process.stdout.write('....');
+app.listen(PORT, HOST, () => {
+  process.stdout.write(`Server listening at -> http://${HOST}:${PORT}\n`);
 });
+
+module.exports = app;
