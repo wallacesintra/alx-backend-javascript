e4d9e719d30f7a651197e9ddae762192dfe5e32a
wallacesintra <wallvxwallace962@gmail.com>
2024-10-01 22:06:01 +0300

    unitest

---
+++ 0x06-unittests_in_js/4-payment.js
@@ -0,0 +1,7 @@
+const Utils = require('./utils');
+
+function sendPaymentRequestToApi(totalAmount, totalShipping) {
+  const res = Utils.calculateNumber('SUM', totalAmount, totalShipping);
+  console.log(`The total is: ${res}`);
+}
+module.exports = sendPaymentRequestToApi;
