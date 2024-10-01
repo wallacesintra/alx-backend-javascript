986f4251efd9c7beffa308c419c4780246d3eb54
wallacesintra <wallvxwallace962@gmail.com>
2024-10-01 21:02:45 +0300

    unittesting in js

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -2,21 +2,23 @@ const assert = require('assert');
 const calculateNumber = require('./0-calcul.js');
 
 describe('calculateNumber', () => {
-	it('checks the output', () => {
-		assert.strictEqual(calculateNumber(1, 3), 4);
-		assert.strictEqual(calculateNumber(1, 3.7), 5);
-		assert.strictEqual(calculateNumber(1.2, 3.7) 5);
-		assert.strictEqual(calculateNumber(1.5, 3.7), 6);
-		assert.strictEqual(calculateNumber(3.7, 1), 5);
-		assert.strictEqual(calculateNumber(3.7, 1.2), 5);
-	});
-	it('negative number', () => {
-		assert.strictEqual(calculateNumber(-1, 1), 0);
-		assert.strictEqual(calculateNumber(1, -1), 0);
-		assert.strictEqual(calculateNumber(-1, -1), -2);
-	});
-	it('checks arguments', () => {
-		assert.strictEqual(isNaN(calculateNumber(1)), true);
-		assert.strictEqual(isNan(calculateNumber()), true);
-	});
+  it('real numbers', () => {
+    assert.strictEqual(calculateNumber(1, 3), 4);
+    assert.strictEqual(calculateNumber(1, 3.7), 5);
+    assert.strictEqual(calculateNumber(2.2, 3.7), 6);
+    assert.strictEqual(calculateNumber(2.5, 3.7), 7);
+    assert.strictEqual(calculateNumber(3.7, 1), 5);
+    assert.strictEqual(calculateNumber(3.7, 1.2), 5);
+  });
+
+  it('negative number', () => {
+    assert.strictEqual(calculateNumber(-1, 1), 0);
+    assert.strictEqual(calculateNumber(1, -1), 0);
+    assert.strictEqual(calculateNumber(-1, -1), -2);
+  });
+
+  it('checks arguments', () => {
+    assert.strictEqual(Number.isNaN(calculateNumber(1)), true);
+    assert.strictEqual(Number.isNaN(calculateNumber()), true);
+  });
 });
